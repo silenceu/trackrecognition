5e836f9cc97b53345cc24dcdf89a87a424aea018
silenceu <szyhs24546@gmail.com>
2017-06-29 22:19:31 +0800

    计算所有排列组合的得分,给出最高的特征集合

---
+++ choose_best.py
@@ -0,0 +1,37 @@
+from itertools import combinations
+from numpy import loadtxt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import numpy as np
+from xgboost.sklearn import XGBClassifier
+
+idx = list(range(6))
+raw_x = loadtxt('train_feature.txt', delimiter=' ')
+labels = loadtxt('labels.txt', delimiter=' ')
+# x_test = loadtxt('train_feature.txt', delimiter=' ')
+seed = 7
+test_size = 0.3
+
+max_idx = list()
+max_score = 0
+
+for i in range(1, 7):
+    sub_idxs = list(combinations(idx, i))
+    for sub_idx in sub_idxs:
+        # print(list(sub_idx), raw_x.shape)
+        x = raw_x[:, list(sub_idx)]
+        # print(len(x))
+        x_train, x_test, y_train, y_test = train_test_split(x, labels, test_size=test_size, random_state=seed)
+        model = XGBClassifier(learning_rate=0.01,
+                              seed=seed,
+                              max_depth=10,
+                              silent=1)
+        model.fit(x_train, y_train)
+        y_pre = model.predict(x_test)
+        predictions = [round(value) for value in y_pre]
+        accuracy = accuracy_score(y_test, predictions)
+        if accuracy > max_score:
+            max_score = accuracy
+            max_idx = np.copy(sub_idx)
+
+print(max_score, max_idx)
