765611596fd987076a67d085ec0a1d8fae0bb491
silenceu <szyhs24546@gmail.com>
2017-06-30 19:29:49 +0800

    测试了下回归模型（630）

--- choose_best.py
+++ choose_best.py
@@ -4,12 +4,17 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 from xgboost.sklearn import XGBClassifier
+from xgboost.sklearn import XGBRegressor
+from remove_abnormal_point import get_result_mark
 
+marks = get_result_mark()
 idx = list(range(6))
-raw_x = loadtxt('train_feature.txt', delimiter=' ')
+raw_x = loadtxt('train_feature630.txt', delimiter=' ')
 labels = loadtxt('labels.txt', delimiter=' ')
+raw_x = raw_x[marks]
+labels = labels[marks]
 # x_test = loadtxt('train_feature.txt', delimiter=' ')
-seed = 7
+seed = 10
 test_size = 0.3
 
 max_idx = list()
@@ -22,10 +27,11 @@ for i in range(1, 7):
         x = raw_x[:, list(sub_idx)]
         # print(len(x))
         x_train, x_test, y_train, y_test = train_test_split(x, labels, test_size=test_size, random_state=seed)
-        model = XGBClassifier(learning_rate=0.01,
-                              seed=seed,
-                              max_depth=10,
-                              silent=1)
+        # model = XGBClassifier(learning_rate=0.01,
+        #                       # seed=seed,
+        #                       max_depth=10,
+        #                       silent=1)
+        model = XGBRegressor(max_depth=10)
         model.fit(x_train, y_train)
         y_pre = model.predict(x_test)
         predictions = [round(value) for value in y_pre]
